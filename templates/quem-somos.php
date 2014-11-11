f3b4fce54136740db4d1fbfcb9a415692ed78d0c
guilhermelf <guilherme.lf@gmail.com>
2014-11-11 16:13:22 -0200

    Update quem-somos.php

--- templates/quem-somos.php
+++ templates/quem-somos.php
@@ -10,125 +10,130 @@
         <link rel="stylesheet" href="assets/css/grids-responsive-min.css">
     </head>
     <body>
-        <div class="content">
-            <h1 class="title-sobre"><a href="index.php">What Should I Do?</a></h1>
-            <br />
-            <div class="pure-g-r">
-                <div class="pure-u-1-24 pure-u-sm-3-4 quem-somos">
-                    <h2 class="h2-sobre">Quem Somos</h2>
+        <div id="wrapper" class="wrapper">
+            <!-- header -->
+                <div id="header">
+                    <?php include "header.php"; ?>
+                </div>
+            <!-- fim do header -->
 
-                    <div class="pure-g-r">
-                        <div class="pure-u-3-8 text-right">
-                            <img src="assets/img/bruno.jpg" alt="" class="img-circle">
+            <div id="content" class="content">
+                <h1 class="title-sobre"><a href="index.php">What Should I Do?</a></h1>
+                <br />
+                <div class="pure-g-r">
+                    <div class="pure-u-1-24 pure-u-sm-3-4 quem-somos">
+                        <h2 class="h2-sobre">Quem Somos</h2>
+
+                        <div class="pure-g-r">
+                            <div class="pure-u-3-8 text-right">
+                                <img src="assets/img/bruno.jpg" alt="" class="img-circle">
+                            </div>
+
+                            <div class="pure-u-5-8">
+                                <h3 class="about-title">
+                                    Bruno Rauber
+                                    <small>Analista de Testes</small>
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
+                                </p>
+                            </div>
                         </div>
 
-                        <div class="pure-u-5-8">
-                            <h3 class="about-title">
-                                Bruno Rauber
-                                <small>Analista de Testes</small>
-                            </h3>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
-                            </p>
+                        <div class="pure-g-r">
+                            <div class="pure-u-3-8 text-right">
+                                <img src="assets/img/cassio.jpg" alt="" class="img-circle">
+                            </div>
+
+                            <div class="pure-u-5-8">
+                                <h3 class="about-title">
+                                    Cassio Couto
+                                    <small>Gerente de projeto</small>
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
+                                </p>
+                            </div>
                         </div>
+
+                        <div class="pure-g-r">
+                            <div class="pure-u-3-8 text-right">
+                                <img src="assets/img/fraga.jpg" alt="" class="img-circle">
+                            </div>
+
+                            <div class="pure-u-5-8">
+                                <h3 class="about-title">
+                                    Guilherme Fraga
+                                    <small>Suporte</small>
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="pure-g-r">
+                            <div class="pure-u-3-8 text-right">
+                                <img src="assets/img/viana.jpg" alt="" class="img-circle">
+                            </div>
+
+                            <div class="pure-u-5-8">
+                                <h3 class="about-title">
+                                    Guilherme Viana
+                                    <small>Back-end developer</small>
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="pure-g-r">
+                            <div class="pure-u-3-8 text-right">
+                                <img src="assets/img/icaro.jpg" alt="" class="img-circle">
+                            </div>
+
+                            <div class="pure-u-5-8">
+                                <h3 class="about-title">
+                                    Ícaro Heimig
+                                    <small>Front-end developer</small>
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
+                                </p>
+                            </div>
+                        </div>
+            
+                        <div class="pure-g-r">
+                            <div class="pure-u-3-8 text-right">
+                                <img src="assets/img/wagner.jpg" alt="" class="img-circle">
+                            </div>
+
+                            <div class="pure-u-5-8">
+                                <h3 class="about-title">
+                                    Wagner Bauer
+                                    <small>Analista de sistemas</small>
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
+                                </p>
+                            </div>
+                        </div>
+
+
                     </div>
-
-                    <div class="pure-g-r">
-                        <div class="pure-u-3-8 text-right">
-                            <img src="assets/img/cassio.jpg" alt="" class="img-circle">
-                        </div>
-
-                        <div class="pure-u-5-8">
-                            <h3 class="about-title">
-                                Cassio Couto
-                                <small>Gerente de projeto</small>
-                            </h3>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="pure-g-r">
-                        <div class="pure-u-3-8 text-right">
-                            <img src="assets/img/fraga.jpg" alt="" class="img-circle">
-                        </div>
-
-                        <div class="pure-u-5-8">
-                            <h3 class="about-title">
-                                Guilherme Fraga
-                                <small>Suporte</small>
-                            </h3>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="pure-g-r">
-                        <div class="pure-u-3-8 text-right">
-                            <img src="assets/img/viana.jpg" alt="" class="img-circle">
-                        </div>
-
-                        <div class="pure-u-5-8">
-                            <h3 class="about-title">
-                                Guilherme Viana
-                                <small>Back-end developer</small>
-                            </h3>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="pure-g-r">
-                        <div class="pure-u-3-8 text-right">
-                            <img src="assets/img/icaro.jpg" alt="" class="img-circle">
-                        </div>
-
-                        <div class="pure-u-5-8">
-                            <h3 class="about-title">
-                                Ícaro Heimig
-                                <small>Front-end developer</small>
-                            </h3>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
-                            </p>
-                        </div>
-                    </div>
-        
-                    <div class="pure-g-r">
-                        <div class="pure-u-3-8 text-right">
-                            <img src="assets/img/wagner.jpg" alt="" class="img-circle">
-                        </div>
-
-                        <div class="pure-u-5-8">
-                            <h3 class="about-title">
-                                Wagner Bauer
-                                <small>Analista de sistemas</small>
-                            </h3>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto exercitationem nisi molestias minus dignissimos, quos optio natus consequatur at quas quisquam nobis obcaecati impedit neque totam nam! Sint, nulla, ipsa.
-                            </p>
-                        </div>
-                    </div>
-
-
+                    <br />
+                    <br />
                 </div>
             </div>
+            
+            <div id="footer" class="footer pure-g">
+                <?php include "footer.html"; ?>
+            </div>
+            <!-- Le Javascripts -->
+            <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+            <script type="text/javascript" src="assets/js/FacebookImport.js"></script>
+            <script type="text/javascript" src="assets/js/home.js"></script>
         </div>
-        
-        <footer class="footer">
-            <div class="footer-data">
-                <div class="footer-itens pure-u-4-6"><a href="sobre">Sobre</a></div>
-                <div class="footer-itens pure-u-4-6"><a href="quem-somos">Quem Somos</a></div>
-                <div class="footer-itens pure-u-4-6"><a href="contato">Contato</a></div>
-                <div class="footer-itens img pure-u-4-6">
-                    <i class="fa fa-facebook-square fa-2x"></i>
-                    <i class="fa fa-twitter-square fa-2x"></i>
-                    <i class="fa fa-github-square fa-2x"></i>
-                </div>
-            </div>
-        </footer>
     </body>
 </html>
