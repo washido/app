0539601b52a0bba48e4366437cdc176feccf3e7b
guilhermelf <guilherme.lf@gmail.com>
2014-11-11 15:52:30 -0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,4 @@
+ <!-- botao do face  esta oculto  -->
 <!-- <fb:login-button class="btn-face" scope="user_actions.videos,user_actions.books,user_actions.music,user_friends" onlogin="checkLoginState();"></fb:login-button> -->
 <!-- opcoes usuário logado  -->
 <div class="pure-u-1 pure-u-md-5-12 user-wrap">
@@ -6,5 +7,5 @@
     </div>
     <div class="user-nome-wrap pure-u-md-1-2">Guilherme Longaray de fraga</div>
     <!-- Faltam os links -->
-    <div class="user-opcoes-wrap pure-u-md-1-3"><a href="">Videos</a> | <a href="">Músicas</a> | <a href="">Livros</a></div>
+    <div class="user-opcoes-wrap pure-u-md-1-3"><a href="/me/movie">Videos</a> | <a href="/me/musics">Músicas</a> | <a href="/me/book">Livros</a></div>
 </div>
