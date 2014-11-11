b79e0fdfaaabd228cb4c9ff2e8e908b4d000ec24
guilhermelf <guilherme.lf@gmail.com>
2014-11-11 15:51:16 -0200

    Create header.php

---
+++ templates/header.php
@@ -0,0 +1,10 @@
+<!-- <fb:login-button class="btn-face" scope="user_actions.videos,user_actions.books,user_actions.music,user_friends" onlogin="checkLoginState();"></fb:login-button> -->
+<!-- opcoes usuário logado  -->
+<div class="pure-u-1 pure-u-md-5-12 user-wrap">
+    <div class="user-img-wrap pure-u-md-1-12">
+        <img width="35px" src="https://graph.facebook.com/guilherme.longaraydefraga/picture">
+    </div>
+    <div class="user-nome-wrap pure-u-md-1-2">Guilherme Longaray de fraga</div>
+    <!-- Faltam os links -->
+    <div class="user-opcoes-wrap pure-u-md-1-3"><a href="">Videos</a> | <a href="">Músicas</a> | <a href="">Livros</a></div>
+</div>
