ae743ef095c7febcfdcc4b9f4a2fe3547696f74b
guilhermelf <guilherme.lf@gmail.com>
2014-12-02 10:49:58 -0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,21 +1,14 @@
 <fb:login-button class="btn-face" scope="user_actions.videos,user_actions.books,user_actions.music,user_friends,user_likes" onlogin="checkLoginState();"></fb:login-button>
 
+<fb:login-button class="btn-face" scope="user_actions.videos,user_actions.books,user_actions.music,user_friends" onlogin="checkLoginState();"></fb:login-button> -->
 <!-- opcoes usuário logado  -->
-<div class="pure-u-1 pure-u-md-5-12 user-wrap">
-    
-    <div class="user-img-wrap pure-u-md-1-12">
-        <img width="35px" src="https://graph.facebook.com/guilherme.longaraydefraga/picture">
-    </div>
-    
-    <div class="user-nome-wrap pure-u-md-1-2">
-        {{ Nome }}    
-    </div>
-    
-    <div class="user-opcoes-wrap pure-u-md-1-3">
-        <a href="/me/movies">Videos</a> 
-        | 
-        <a href="/me/musics">Músicas</a> 
-        | 
-        <a href="/me/books">Livros</a>
-    </div>
+<div class="pure-g-r">
+	<div class="pure-u-1 pure-u-lg-11-24 user-wrap">
+	    <div class="user-img-wrap">
+	        <img width="35px" src="https://graph.facebook.com/guilherme.longaraydefraga/picture">
+	    </div>
+	    <div class="pure-u-1-3">Guilherme Longaray</div>
+	    <!-- Faltam os links -->
+	    <div class="user-opcoes-wrap pure-u-11-24"><a href="/me/movie">Videos</a> | <a href="/me/music">Músicas</a> | <a href="/me/book">Livros</a>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">Sair</a></div>
+	</div>
 </div>
